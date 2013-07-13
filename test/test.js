8848ca91ed0def188cf4eebbaf8757920a7eff08
Tong ZHANG <kanetzhang@gmail.com>
2013-07-13 11:16:34 +0800

    add mocha

---
+++ test/test.js
@@ -0,0 +1,13 @@
+describe("First test", function () {
+    describe("True", function () {
+        it("should be true", function () {
+            true.should.be.true;
+        });
+    });
+
+    describe("False", function () {
+        it("should be false", function () {
+            false.should.be.false;
+        });
+    });
+});
