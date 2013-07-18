91dad5a70cadee6a508580bdd1c910a14ec1df9e
swzhou <swing.zhou@hotmail.com>
2013-07-18 22:50:24 +0800

    add message model

--- db/schema.js
+++ db/schema.js
@@ -9,17 +9,28 @@
  Example of model definition:
 
  define('User', function () {
-     property('email', String, { index: true });
-     property('password', String);
-     property('activated', Boolean, {default: false});
+ property('email', String, { index: true });
+ property('password', String);
+ property('activated', Boolean, {default: false});
  });
 
  Example of schema configured without config/database.json (heroku redistogo addon):
  schema('redis', {url: process.env.REDISTOGO_URL}, function () {
-     // model definitions here
+ // model definitions here
  });
 
-*/
+ */
+
+var Message = describe('Message', function() {
+    property('rawContent', String);
+    property('fromUser', String);
+    property('toUser', String);
+    property('type', String);
+    property('content', String);
+    property('createTime', {type: Date, default: function() {
+        return new Date();
+    }});
+});
 
 var User = describe('User', function () {
     property('id', String, { index: true });
